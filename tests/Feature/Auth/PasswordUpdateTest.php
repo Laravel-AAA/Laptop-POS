d1c3bc98646dc8da497f7918c17e5766a4cda221
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-09-17 21:12:30 +0300

    fix test default password

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -19,7 +19,7 @@ class PasswordUpdateTest extends TestCase
             ->actingAs($user)
             ->from('/profile')
             ->put('/password', [
-                'current_password' => 'password',
+                'current_password' => 'asdfasdf',
                 'password' => 'new-password',
                 'password_confirmation' => 'new-password',
             ]);
