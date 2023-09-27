8994eaac4a926240b9f3285374b561424cc189ee
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-09-27 03:25:53 +0300

    display `Nothing found` if so

--- resources/js/Pages/Inventory/index.tsx
+++ resources/js/Pages/Inventory/index.tsx
@@ -9,21 +9,22 @@ import { Head } from "@inertiajs/react";
 import Product from "./Partials/Product";
 import TextInput from "@/Components/TextInput";
 import { useState } from "react";
-import { FaPlus } from "react-icons/fa";
+import { FaPlus  } from "react-icons/fa";
 import SecondaryButton from "@/Components/SecondaryButton";
 import AddEditProductModal from "@/Components/Modals/Add-Edit/AddEditProductModal";
 import Pagination from "@/Components/Pagination";
+import { BsSearch } from "react-icons/bs";
 
 export default function Inventory({
   auth,
-  products: paginateProducts
+  products: paginateProducts,
 }: PageProps<{ products: ILaravelPaginate<IProduct> }>) {
   const [searchValue, setSearch] = useState("");
   const [productModalAction, setProductModalAction] =
     useState<ProductModalAction>("create");
   const [isAddEditProductModal, setAddEditProductModal] = useState(false);
   const products: IProduct[] = paginateProducts.data;
-  
+
   return (
     <>
       <AddEditProductModal
@@ -38,18 +39,9 @@ export default function Inventory({
             <label className="relative block flex-grow">
               <span className="sr-only">Search</span>
               <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <svg
-                  className="h-5 w-5 fill-slate-500"
-                  viewBox="0 0 50 50"
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
-                  y="0px"
-                  width="100"
-                  height="100"
-                >
-                  <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
-                </svg>
+                <BsSearch />
               </span>
+
               <TextInput
                 id="search"
                 type="search"
@@ -78,6 +70,9 @@ export default function Inventory({
       >
         <Head title="Inventory" />
         <div className="flex flex-wrap  justify-center py-6">
+          {products.filter((p) =>
+            p.name.toLowerCase().includes(searchValue.toLowerCase()),
+          ).length == 0 && <div className="flex gap-4 opacity-50 my-20"><BsSearch className="mt-1"/><p>No products found!</p></div>}
           {products
             .filter((p) =>
               p.name.toLowerCase().includes(searchValue.toLowerCase()),
