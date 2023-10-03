f65ea4721d3d02426ee30ef8dd55cd5557f3bd02
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-03 12:45:44 +0300

    build

---
+++ public/build/assets/InputLabel-e9437e65.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-c9e5f867.js";function r({value:t,className:e="",children:s,...m}){return n.jsx("label",{...m,className:"block font-medium text-sm text-slate-600 "+e,children:t||s})}export{r as I};
