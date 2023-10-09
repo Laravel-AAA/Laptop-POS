68d42bc411e501b82c14f0ca3c3e4afadf65abd7
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-09 17:54:23 +0300

    build

---
+++ public/build/assets/PrimaryButton-e6ec4349.js
@@ -0,0 +1,9 @@
+import{j as n}from"./app-a8cc043f.js";function u({type:r="button",className:t="",disabled:i,children:e,...o}){return n.jsx("button",{...o,type:r,className:`inline-block cursor-pointer items-center rounded-md border
+        border-transparent bg-primary-800 px-4
+        py-2 text-center  text-xs font-semibold
+        uppercase tracking-widest
+        text-white transition
+        duration-200 ease-in-out hover:bg-primary-700 focus:outline-none  focus:ring-2
+        focus:ring-primary-400 focus:ring-offset-2 focus-visible:outline-none focus-visible:ring-2
+        focus-visible:ring-primary-400 focus-visible:ring-offset-2
+         active:scale-95 disabled:opacity-25 ${i&&"opacity-25"} `+t,disabled:i,children:e})}export{u as P};
