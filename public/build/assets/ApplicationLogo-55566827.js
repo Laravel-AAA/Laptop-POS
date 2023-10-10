368a381fd8b4fba2738d25088f24a3ebbbc77fbf
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-10 14:50:41 +0300

    build

--- public/build/assets/ApplicationLogo-55566827.js
+++
@@ -1,1 +0,0 @@
-import{j as p}from"./app-34bdad78.js";function i({className:o}){return p.jsx("img",{className:o,src:"/logo/laptop-pos-logo-high.png"})}export{i as A};
---
+++ public/build/assets/ApplicationLogo-614c54c3.js
@@ -0,0 +1,1 @@
+import{j as p}from"./app-7db9876e.js";function i({className:o}){return p.jsx("img",{className:o,src:"/logo/laptop-pos-logo-high.png"})}export{i as A};
