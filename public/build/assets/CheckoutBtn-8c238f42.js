059f053577862152a1361dcea6064251ed951f84
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-18 02:47:34 +0300

    build

--- public/build/assets/CheckoutBtn-8c238f42.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-a6e2ab9e.js";function e(){return t.jsx("div",{className:"bg-brown-500 mt-20 bottom-0 w-full h-20",children:"CheckoutBtn"})}export{e as default};
---
+++ public/build/assets/CheckoutBtn-f7a827ab.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-f8210ef9.js";function e(){return t.jsx("div",{className:"bg-brown-500 mt-20 bottom-0 w-full h-20",children:"CheckoutBtn"})}export{e as default};
