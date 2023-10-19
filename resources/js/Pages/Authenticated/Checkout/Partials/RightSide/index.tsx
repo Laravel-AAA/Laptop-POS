c07a046f15e053a389d647144eedff778dba7886
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-20 01:22:23 +0300

    make Checkout page responsive

--- resources/js/Pages/Authenticated/Checkout/Partials/RightSide/index.tsx
+++ resources/js/Pages/Authenticated/Checkout/Partials/RightSide/index.tsx
@@ -15,13 +15,20 @@ export default function RightSide({
 }) {
   const [scrolled, setScrolled] = useState<number>(0);
 
-
+  /**The scrolled useState and useEffect is because `sticky` RightSide has height of screen hight
+   * (if 100% it will follow the whole page height its stickiness is useless).
+   * And we want the Checkout button at the bottom but the header take about 47px height,
+   * so we have to change the height of the RightSide component so that its height
+   * is (screen height - header height) which will make the button looks at bottom.
+   * The problem is when scrolling the header height is gone, so we increase the height
+   * of RightSide so that it become (screen height) and the Checkout button is at the bottom.
+   */
   useEffect(() => {
     function handleSetScroll() {
       console.log("scroll", window.scrollY);
       console.log(scrolled);
       setScrolled(window.scrollY);
-    };
+    }
     handleSetScroll();
     window.addEventListener("scroll", handleSetScroll);
     return () => window.removeEventListener("scroll", handleSetScroll);
@@ -31,7 +38,7 @@ export default function RightSide({
       style={{
         height: scrolled > 50 ? "100%" : `calc(100% - ${50 - scrolled}px)`,
       }}
-      className={`relative flex flex-col bg-gray-200 px-4 transition duration-700 ease-in-out md:px-0 ${className}`}
+      className={`relative flex flex-col bg-gray-200 mx-4 transition-all duration-300 ease-in-out md:px-0 ${className}`}
     >
       <TotalInfo taxPercent={taxPercent} billOperations={billOperations} />
       <CartInfo taxPercent={taxPercent} billOperations={billOperations} />
