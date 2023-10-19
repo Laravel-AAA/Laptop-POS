c29ac2ace86ee695d3bfe36d90656b373a48b3dc
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-19 21:46:13 +0300

    build

--- public/build/assets/TextInput-11c866e3.js
+++
@@ -1,9 +0,0 @@
-import{r,j as a}from"./app-a134fab9.js";const u=r.forwardRef(function({type:t="text",className:f="",isFocused:l=!1,...o},s){const i=r.useRef(null);return r.useImperativeHandle(s,()=>({focus:()=>{var e;return(e=i.current)==null?void 0:e.focus()}})),r.useEffect(()=>{var e;l&&((e=i.current)==null||e.focus())},[]),a.jsx("input",{...o,type:t,className:`placeholder:text-slate-400 rounded-md border  border-gray-300 shadow-sm
-        file:mr-3 file:cursor-pointer file:rounded-md
-          file:rounded-e-none file:border file:border-transparent file:bg-primary-700 file:px-4
-        file:py-[0.69rem] file:text-center file:text-xs
-        file:font-semibold file:uppercase file:tracking-widest file:text-white
-        file:transition file:duration-200 file:ease-in-out
-        placeholder:italic file:hover:bg-primary-600
-        focus:border-primary-600 focus:ring-primary-600 file:active:scale-95
-         disabled:opacity-60  `+f,ref:i})});export{u as T};
