c6fd7aa39b856f7106a48679e2019e79b8ab68f4
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-22 17:32:23 +0300

    fix inventory header

--- resources/js/Pages/Authenticated/Inventory/Partials/InventoryHeader.tsx
+++ resources/js/Pages/Authenticated/Inventory/Partials/InventoryHeader.tsx
@@ -41,7 +41,7 @@ export default function InventoryHeader({
   }, [filter]);
 
   return (
-    <div className="block justify-between py-4 sm:flex">
+    <div className="block justify-between py-4 md:flex">
       <div className="flex items-center gap-3">
         <h2 className="mr-6 text-xl font-semibold leading-tight text-gray-800">
           Inventory
@@ -60,7 +60,7 @@ export default function InventoryHeader({
             autoComplete="off"
             value={filter.search}
             isFocused={true}
-            className="w-full p-1 pl-9 sm:w-72 "
+            className="w-full p-1 pl-9 md:w-72 "
             placeholder="Search for products..."
             onChange={(v) =>
               setFilter((p) => ({ ...p, search: v.target.value }))
@@ -68,12 +68,23 @@ export default function InventoryHeader({
           />
         </label>
         {filter.search && prevFilter?.search === filter.search && (
-          <TotalResult text="Result" number={totalResult} />
+          <TotalResult
+            className="hidden md:block"
+            text="Result"
+            number={totalResult}
+          />
         )}
       </div>
-      <div className="mt-3 self-center sm:mt-0">
+      <div className="mt-3 flex justify-end md:mt-0 md:block">
         {!filter.search && prevFilter?.search === filter.search && (
-          <TotalResult text="Total" number={totalResult} />
+          <TotalResult className="md:mt-0 mt-1" text="Total" number={totalResult} />
+        )}
+        {filter.search && prevFilter?.search === filter.search && (
+          <TotalResult
+            className="block md:hidden md:mt-0 mt-1"
+            text="Result"
+            number={totalResult}
+          />
         )}
         <SecondaryButton
           className="inline-flex pl-2 pr-2"
@@ -91,12 +102,14 @@ export default function InventoryHeader({
 function TotalResult({
   text,
   number,
+  className = "",
 }: {
   text: "Total" | "Result";
   number: number;
+  className?: string;
 }) {
   return (
-    <p className="m-0 mr-3 inline p-0 text-center text-gray-600">
+    <p className={"m-0 mr-3 inline p-0 text-center text-gray-600 " + className}>
       {text}:&nbsp;
       <span className="text-gray-900">{number}</span>
     </p>
