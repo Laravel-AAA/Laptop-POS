b49d7f97a093ba6ed7e8941fe0123efe641a6ded
Ahmad Alkaf <Ahmad.Alkaf.AHK@gmail.com>
2023-10-29 10:08:33 +0300

    fix Invalid Signature of the verification url

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,6 +13,7 @@ use App\Policies\TransactionPolicy;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -36,9 +37,10 @@ class AuthServiceProvider extends ServiceProvider
             return (new MailMessage)
                 ->subject('Verify Email Address')
                 ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address', $url);
+                ->action('Verify Email Address', $url)
+                ->line(Lang::get('If you did not create an account, no further action is required.'));
         });
 
-        
+
     }
 }
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{env('APP_URL')}}/assets/logo/laptop-pos-logo-text.png" class="logo" alt="Laptop POS Logo">
+            <img src="{{env('APP_URL')}}/assets/logo/laptop-pos-logo-text.png" class="logo" alt="Laptop POS">
         </a>
     </td>
 </tr>
